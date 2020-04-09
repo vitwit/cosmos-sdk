7be8b7546a75cd5a5109a95434adb0e9f9a8b136
sahith-narahari <sahithnarahari@gmail.com>
2020-04-09 23:58:31 +0530

    Migrated keeper tests to proto

--- x/msg_authorization/types/send_authorization.go
+++ x/msg_authorization/types/send_authorization.go
@@ -10,18 +10,18 @@ func (authorization SendAuthorization) MsgType() string {
 	return bank.MsgSend{}.Type()
 }
 
-func (authorization SendAuthorization) Accept(msg sdk.Msg, block abci.Header) (allow bool, updated AuthorizationI, delete bool) {
+func (authorization SendAuthorization) Accept(msg sdk.Msg, block abci.Header) (allow bool, updated Authorization, delete bool) {
 	switch msg := msg.(type) {
 	case bank.MsgSend:
 		limitLeft, isNegative := authorization.SpendLimit.SafeSub(msg.Amount)
 		if isNegative {
-			return false, nil, false
+			return false, Authorization{}, false
 		}
 		if limitLeft.IsZero() {
-			return true, nil, true
+			return true, Authorization{}, true
 		}
 
-		return true, SendAuthorization{SpendLimit: limitLeft}, false
+		return true, Authorization{Sum: &Authorization_SendAuthorization{SendAuthorization: &SendAuthorization{SpendLimit: limitLeft}}}, false
 	}
-	return false, nil, false
+	return false, Authorization{}, false
 }
